cdf2149a43e301dba06f6b3168a41484c37a67d4
komal menaria <komalmenariacpm2003@gmail.com>
2022-12-10 23:42:51 +0530

    onclick funcationality added to dishes and lifting state Up

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
+import { useState } from 'react';
 import './App.css';
 import { Navbar, NavbarBrand} from 'reactstrap'
 import Menu from './components/Menu';
-
+import {DISHES} from './shared/dishes'
 function App() {
+  const [dishes, setDishes] = useState(DISHES);
+  
+
   return (
-    <div >
-     <Navbar dark color={'dark'}>
+    <>
+    <Navbar dark color={'dark'}>
       <div className='container'>
         <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
       </div>
      </Navbar>
-     <Menu />
-    </div>
+     <Menu dishes={dishes} />
+    </>
+     
+   
   );
 }
 
