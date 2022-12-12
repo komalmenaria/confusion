5bf21ef276e0ca25f01e429fe7e3965ca132f03b
komal menaria <komalmenariacpm2003@gmail.com>
2022-12-12 18:36:36 +0530

    presentational and container component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,13 @@
-import { useState } from 'react';
+import {React} from 'react';
 import './App.css';
-import { Navbar, NavbarBrand} from 'reactstrap'
-import Menu from './components/Menu';
-import {DISHES} from './shared/dishes'
+import Main from './components/Main';
+
 function App() {
-  const [dishes, setDishes] = useState(DISHES);
-  
+ 
 
   return (
     <>
-    <Navbar dark color={'dark'}>
-      <div className='container'>
-        <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-      </div>
-     </Navbar>
-     <Menu dishes={dishes} />
+    <Main />
     </>
      
    
---
+++ src/components/Main.js
@@ -0,0 +1,38 @@
+import { useState } from 'react';
+import Menu from './Menu';
+import { Navbar, NavbarBrand} from 'reactstrap'
+import {DISHES} from '../shared/dishes'
+import Dishdetail from './Dishdetail';
+
+function Main() {
+  const [dishes, setDishes] = useState(DISHES);
+  const [selectedDish, setSelectedDish] = useState(null);
+  
+  function onDishSelect(dishID){
+    setSelectedDish(dishID);
+  };
+
+  
+
+  return (
+    <>
+     <Navbar dark color={'dark'}>
+      <div className='container'>
+        <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+      </div>
+     </Navbar>
+     <Menu dishes={dishes} onClick={(dishID) => onDishSelect(dishID)} />
+     <div className="container">
+      <div className="row">
+      <Dishdetail dish={dishes.filter((dish)=> dish.id === selectedDish)[0]} />
+
+      </div>
+
+     </div>
+    </>
+     
+   
+  );
+}
+
+export default Main;
